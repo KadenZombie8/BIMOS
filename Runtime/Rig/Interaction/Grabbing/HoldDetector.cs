719407afdd0215d160090c35967d1966eaf0cdda
Kaden <kadenzombie8@gmail.com>
2025-11-22 22:41:43 +0000

    Updated collision ignorer to use HoldDetector

--- Runtime/Rig/Interaction/Grabbing/HoldDetector.cs
+++ Runtime/Rig/Interaction/Grabbing/HoldDetector.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using KadenZombie8.BIMOS.Sockets;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,158 +8,82 @@ using UnityEngine.Events;
 namespace KadenZombie8.BIMOS.Rig
 {
     /// <summary>
-    /// Detects when an object (through multiple grabs) is first held or finally released.
+    /// Detects when an object (through multiple grabs) is first held or last released.
     /// </summary>
+    [RequireComponent(typeof(Item))]
     public class HoldDetector : MonoBehaviour
     {
         public UnityEvent OnFirstGrab;
-        public UnityEvent OnFinalRelease;
-
-        private readonly Dictionary<Socket, (Action, Action)> _socketListeners = new();
+        public UnityEvent OnLastRelease;
 
         private readonly HashSet<Grabbable> _grabbables = new();
-        private readonly HashSet<Socket> _sockets = new();
-        private readonly HashSet<Collider> _colliders = new();
-        private readonly HashSet<Collider> _playerColliders = new();
+        private Item _item;
+        private bool _wasHolding;
 
-        private void Awake()
+        private void Awake() => _item = GetComponent<Item>();
+
+        private void AddGrabbable(Grabbable grabbable)
         {
-            foreach (var grabbable in GetComponentsInChildren<Grabbable>())
-                _grabbables.Add(grabbable);
-            foreach (var socket in GetComponentsInChildren<Socket>())
-                _sockets.Add(socket);
-            foreach (var collider in GetComponentsInChildren<Collider>())
-                _colliders.Add(collider);
+            _grabbables.Add(grabbable);
+            grabbable.OnGrab.AddListener(CheckIsHolding);
+            grabbable.OnRelease.AddListener(CheckIsHolding);
         }
 
-        private void Start()
+        private void RemoveGrabbable(Grabbable grabbable)
         {
-            var colliders = BIMOSRig.Instance.PhysicsRig.Colliders;
-            _playerColliders.Add(colliders.Head);
-            _playerColliders.Add(colliders.Body);
-            _playerColliders.Add(colliders.LocomotionSphere);
+            _grabbables.Remove(grabbable);
+            grabbable.OnGrab.RemoveListener(CheckIsHolding);
+            grabbable.OnRelease.RemoveListener(CheckIsHolding);
         }
 
         private void OnEnable()
         {
-            foreach (var grabbable in _grabbables)
+            foreach (GameObject gameObject in _item.GameObjects)
             {
-                grabbable.OnGrab.AddListener(CheckGrabbables);
-                grabbable.OnRelease.AddListener(CheckGrabbables);
+                foreach (var grabbable in gameObject.GetComponentsInChildren<Grabbable>())
+                    AddGrabbable(grabbable);
             }
 
-            foreach (var socket in _sockets)
-            {
-                void attachListener() => UpdateSocketColliders(socket, true);
-                void detachListener() => UpdateSocketColliders(socket, false);
-
-                _socketListeners[socket] = (attachListener, detachListener);
-
-                socket.OnAttach += attachListener;
-                socket.OnDetach += detachListener;
-            }
+            _item.OnGameObjectAdded += GameObjectAdded;
+            _item.OnGameObjectRemoved += GameObjectRemoved;
         }
 
         private void OnDisable()
         {
-            IgnorePlayerCollision(false);
+            foreach (var grabbable in _grabbables.ToArray())
+                RemoveGrabbable(grabbable);
 
-            foreach (var grabbable in _grabbables)
-            {
-                grabbable.OnGrab.RemoveListener(CheckGrabbables);
-                grabbable.OnRelease.RemoveListener(CheckGrabbables);
-            }
-
-            foreach (var socket in _sockets)
-            {
-                if (_socketListeners.TryGetValue(socket, out var pair))
-                {
-                    socket.OnAttach -= pair.Item1;
-                    socket.OnDetach -= pair.Item2;
-                }
-            }
-
-            _socketListeners.Clear();
+            _item.OnGameObjectAdded -= GameObjectAdded;
+            _item.OnGameObjectRemoved -= GameObjectRemoved;
         }
 
-        private void UpdateSocketColliders(Socket socket, bool isAttaching)
+        private void GameObjectAdded(GameObject gameObject)
         {
-            var plug = socket.Plug;
-            if (!plug)
-                return;
-
-            var rigidbody = plug.Rigidbody;
-            var colliders = rigidbody.GetComponentsInChildren<Collider>();
-            var grabbables = rigidbody.GetComponentsInChildren<Grabbable>();
-
-            if (isAttaching)
-            {
-                foreach (var collider in colliders)
-                    _colliders.Add(collider);
-
-                foreach (var grabbable in grabbables)
-                {
-                    _grabbables.Add(grabbable);
-                    grabbable.OnGrab.AddListener(CheckGrabbables);
-                    grabbable.OnRelease.AddListener(CheckGrabbables);
-                }
-            }
-            else
-            {
-                foreach (var collider in colliders)
-                    foreach (var playerCollider in _playerColliders)
-                        Physics.IgnoreCollision(collider, playerCollider, false);
-
-                foreach (var collider in colliders)
-                    _colliders.Remove(collider);
-
-                foreach (var grabbable in grabbables)
-                {
-                    _grabbables.Remove(grabbable);
-                    grabbable.OnGrab.RemoveListener(CheckGrabbables);
-                    grabbable.OnRelease.RemoveListener(CheckGrabbables);
-                }
-            }
-
-            CheckGrabbables();
+            foreach (var grabbable in gameObject.GetComponentsInChildren<Grabbable>())
+                AddGrabbable(grabbable);
         }
 
-        private void CheckGrabbables()
+        private void GameObjectRemoved(GameObject gameObject)
+        {
+            foreach (var grabbable in gameObject.GetComponentsInChildren<Grabbable>())
+                RemoveGrabbable(grabbable);
+        }
+
+        private void CheckIsHolding()
+        {
+            var isHolding = IsHolding();
+            if (isHolding == _wasHolding) return;
+
+            (isHolding ? OnFirstGrab : OnLastRelease)?.Invoke();
+            _wasHolding = isHolding;
+        }
+
+        private bool IsHolding()
         {
             foreach (var grabbable in _grabbables)
-            {
-                if (grabbable.LeftHand != grabbable.RightHand)
-                {
-                    IgnorePlayerCollision(true);
-                    return;
-                }
-            }
-            foreach (var socket in _sockets)
-            {
-                var plug = socket.Plug;
-                if (!plug)
-                    continue;
+                if (grabbable.LeftHand != grabbable.RightHand) return true;
 
-                var rigidbody = plug.Rigidbody;
-                var grabbables = rigidbody.GetComponentsInChildren<Grabbable>();
-                foreach (var grabbable in grabbables)
-                {
-                    if (grabbable.LeftHand != grabbable.RightHand)
-                    {
-                        IgnorePlayerCollision(true);
-                        return;
-                    }
-                }
-            }
-            IgnorePlayerCollision(false);
-        }
-
-        private void IgnorePlayerCollision(bool ignore)
-        {
-            foreach (var collider in _colliders)
-                foreach (var playerCollider in _playerColliders)
-                    if (collider && playerCollider)
-                        Physics.IgnoreCollision(collider, playerCollider, ignore);
+            return false;
         }
     }
 }
--- Runtime/Rig/Interaction/Grabbing/PlayerCollisionIgnorer.cs
+++ Runtime/Rig/Interaction/Grabbing/PlayerCollisionIgnorer.cs
@@ -1,34 +1,43 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using KadenZombie8.BIMOS.Rig;
-using KadenZombie8.BIMOS.Sockets;
 using UnityEngine;
 
 namespace KadenZombie8.BIMOS.Guns
 {
     /// <summary>
-    /// Makes the gun ignore player collision (good for two-handed weapons)
+    /// Makes the item and any connected objects ignore player collision (good for two-handed weapons)
     /// </summary>
+    [RequireComponent(typeof(Item))]
     public class PlayerCollisionIgnorer : MonoBehaviour
     {
-        public event Action OnGrabbed;
-        public event Action OnReleased;
-
-        private readonly Dictionary<Socket, (Action, Action)> _socketListeners = new();
-
-        private readonly HashSet<Grabbable> _grabbables = new();
-        private readonly HashSet<Socket> _sockets = new();
         private readonly HashSet<Collider> _colliders = new();
         private readonly HashSet<Collider> _playerColliders = new();
 
-        private void Awake()
+        private Item _item;
+        private bool _ignore;
+
+        private void Awake() => _item = GetComponent<Item>();
+
+        private void OnEnable()
         {
-            foreach (var grabbable in GetComponentsInChildren<Grabbable>())
-                _grabbables.Add(grabbable);
-            foreach (var socket in GetComponentsInChildren<Socket>())
-                _sockets.Add(socket);
-            foreach (var collider in GetComponentsInChildren<Collider>())
-                _colliders.Add(collider);
+            foreach (GameObject gameObject in _item.GameObjects)
+            {
+                foreach (var collider in gameObject.GetComponentsInChildren<Collider>())
+                    AddCollider(collider);
+            }
+
+            _item.OnGameObjectAdded += GameObjectAdded;
+            _item.OnGameObjectRemoved += GameObjectRemoved;
+        }
+
+        private void OnDisable()
+        {
+            foreach (var collider in _colliders.ToArray())
+                RemoveCollider(collider);
+
+            _item.OnGameObjectAdded -= GameObjectAdded;
+            _item.OnGameObjectRemoved -= GameObjectRemoved;
         }
 
         private void Start()
@@ -39,125 +48,42 @@ namespace KadenZombie8.BIMOS.Guns
             _playerColliders.Add(colliders.LocomotionSphere);
         }
 
-        private void OnEnable()
+        public void SetIgnorePlayerCollision(bool ignore)
         {
-            foreach (var grabbable in _grabbables)
-            {
-                grabbable.OnGrab.AddListener(CheckGrabbables);
-                grabbable.OnRelease.AddListener(CheckGrabbables);
-            }
-
-            foreach (var socket in _sockets)
-            {
-                void attachListener() => UpdateSocketColliders(socket, true);
-
-                void detachListener() => UpdateSocketColliders(socket, false);
-
-                _socketListeners[socket] = (attachListener, detachListener);
-
-                socket.OnAttach += attachListener;
-                socket.OnDetach += detachListener;
-            }
+            _ignore = ignore;
+            foreach (Collider collider in _colliders)
+                IgnorePlayerCollision(collider, ignore);
         }
 
-        private void OnDisable()
+        private void GameObjectAdded(GameObject gameObject)
         {
-            IgnorePlayerCollision(false, _colliders);
-
-            foreach (var grabbable in _grabbables)
-            {
-                grabbable.OnGrab.RemoveListener(CheckGrabbables);
-                grabbable.OnRelease.RemoveListener(CheckGrabbables);
-            }
-
-            foreach (var socket in _sockets)
-            {
-                if (_socketListeners.TryGetValue(socket, out var pair))
-                {
-                    socket.OnAttach -= pair.Item1;
-                    socket.OnDetach -= pair.Item2;
-                }
-            }
-
-            _socketListeners.Clear();
+            foreach (var collider in gameObject.GetComponentsInChildren<Collider>())
+                AddCollider(collider);
         }
 
-        private void UpdateSocketColliders(Socket socket, bool isAttaching)
+        private void GameObjectRemoved(GameObject gameObject)
         {
-            var plug = socket.Plug;
-            if (!plug)
-                return;
-
-            var plugRigidbody = plug.Rigidbody;
-            var plugColliders = plugRigidbody.GetComponentsInChildren<Collider>();
-            var plugGrabbables = plugRigidbody.GetComponentsInChildren<Grabbable>();
-
-            if (isAttaching)
-            {
-                foreach (var plugCollider in plugColliders)
-                    _colliders.Add(plugCollider);
-
-                foreach (var plugGrabbable in plugGrabbables)
-                {
-                    _grabbables.Add(plugGrabbable);
-                    plugGrabbable.OnGrab.AddListener(CheckGrabbables);
-                    plugGrabbable.OnRelease.AddListener(CheckGrabbables);
-                }
-            }
-            else
-            {
-                IgnorePlayerCollision(false, plugColliders);
-
-                foreach (var plugCollider in plugColliders)
-                    _colliders.Remove(plugCollider);
-
-                foreach (var plugGrabbable in plugGrabbables)
-                {
-                    _grabbables.Remove(plugGrabbable);
-                    plugGrabbable.OnGrab.RemoveListener(CheckGrabbables);
-                    plugGrabbable.OnRelease.RemoveListener(CheckGrabbables);
-                }
-            }
-
-            CheckGrabbables();
+            foreach (var collider in gameObject.GetComponentsInChildren<Collider>())
+                RemoveCollider(collider);
         }
 
-        private void CheckGrabbables()
+        private void IgnorePlayerCollision(Collider collider, bool ignore)
         {
-            foreach (var grabbable in _grabbables)
-            {
-                if (grabbable.LeftHand != grabbable.RightHand)
-                {
-                    IgnorePlayerCollision(true, _colliders);
-                    return;
-                }
-            }
-            foreach (var socket in _sockets)
-            {
-                var plug = socket.Plug;
-                if (!plug)
-                    continue;
-
-                var rigidbody = plug.Rigidbody;
-                var grabbables = rigidbody.GetComponentsInChildren<Grabbable>();
-                foreach (var grabbable in grabbables)
-                {
-                    if (grabbable.LeftHand != grabbable.RightHand)
-                    {
-                        IgnorePlayerCollision(true, _colliders);
-                        return;
-                    }
-                }
-            }
-            IgnorePlayerCollision(false, _colliders);
+            foreach (var playerCollider in _playerColliders)
+                if (collider && playerCollider)
+                    Physics.IgnoreCollision(collider, playerCollider, ignore);
         }
 
-        private void IgnorePlayerCollision(bool ignore, IEnumerable<Collider> colliders)
+        private void AddCollider(Collider collider)
         {
-            foreach (var collider in colliders)
-                foreach (var playerCollider in _playerColliders)
-                    if (collider && playerCollider)
-                        Physics.IgnoreCollision(collider, playerCollider, ignore);
+            _colliders.Add(collider);
+            IgnorePlayerCollision(collider, _ignore);
+        }
+
+        private void RemoveCollider(Collider collider)
+        {
+            _colliders.Remove(collider);
+            IgnorePlayerCollision(collider, false);
         }
     }
 }
