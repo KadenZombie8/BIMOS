73306a8f5f841354ac22bcb735b9b6da57054307
KadenZombie8 <kadenzombie8@gmail.com>
2026-02-12 12:48:14 +0000

    Fixed discrete crouch look height
    Prevented looking up and down from affecting your crouch height while standing

--- Runtime/Rig/Movement/Crouching/DiscreteCrouch.cs
+++ Runtime/Rig/Movement/Crouching/DiscreteCrouch.cs
@@ -6,6 +6,7 @@ namespace KadenZombie8.BIMOS.Rig.Movement
     {
         private Crouching _crouching;
         private VirtualCrouching _virtualCrouching;
+        private ControllerRig _controllerRig;
 
         private void Awake()
         {
@@ -13,13 +14,23 @@ namespace KadenZombie8.BIMOS.Rig.Movement
             _virtualCrouching = GetComponent<VirtualCrouching>();
         }
 
+        private void Start()
+        {
+            _controllerRig = BIMOSRig.Instance.ControllerRig;
+        }
+
         private void FixedUpdate()
         {
+            var neckYDifference = _controllerRig.Transforms.Camera.position.y - _controllerRig.Transforms.HeadCameraOffset.position.y;
+            var minLegHeight = _crouching.CrawlingLegHeight - neckYDifference;
+            var maxLegHeight = _crouching.StandingLegHeight - neckYDifference;
+
             var isCrouching = _virtualCrouching.IsCrouchChanging && _virtualCrouching.CrouchInputMagnitude < 0f;
             var sign = isCrouching ? -1f : 1f;
             var fullHeight = _crouching.StandingLegHeight - _crouching.CrouchingLegHeight;
             var crouchChange = sign * _virtualCrouching.CrouchSpeed * fullHeight * Time.fixedDeltaTime;
-            _crouching.TargetLegHeight = Mathf.Clamp(_crouching.TargetLegHeight + crouchChange, _crouching.CrawlingLegHeight, _crouching.StandingLegHeight);
+
+            _crouching.TargetLegHeight = Mathf.Clamp(_crouching.TargetLegHeight + crouchChange, minLegHeight, maxLegHeight);
         }
     }
 }
