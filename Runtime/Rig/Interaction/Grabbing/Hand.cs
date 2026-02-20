40568d19f76a70d52acc301233b278fe72a8d107
KadenZombie8 <kadenzombie8@gmail.com>
2026-02-20 18:56:46 +0000

    Added gamepad haptics

--- Runtime/Rig/Interaction/Grabbing/Hand.cs
+++ Runtime/Rig/Interaction/Grabbing/Hand.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using KadenZombie8.BIMOS.Rig.Movement;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -21,14 +22,33 @@ namespace KadenZombie8.BIMOS.Rig
         public ArmColliders ArmColliders;
         public Joint GrabJoint;
 
-        [SerializeField]
-        private InputActionReference _hapticAction;
+        private Coroutine _hapticCoroutine;
 
         public void SendHapticImpulse(float amplitude, float duration)
         {
+            // XR
             var device = Handedness == Handedness.Left ? InputDevices.GetDeviceAtXRNode(XRNode.LeftHand) : InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
             if (device.isValid)
                 device.SendHapticImpulse(0, amplitude, duration);
+
+            // Gamepad
+            var gamepad = Gamepad.current;
+            if (gamepad != null)
+            {
+                gamepad.SetMotorSpeeds(amplitude, amplitude);
+
+                if (_hapticCoroutine != null)
+                    StopCoroutine(_hapticCoroutine);
+
+                _hapticCoroutine = StartCoroutine(StopHaptics(duration, gamepad));
+            }
+        }
+
+        private IEnumerator StopHaptics(float duration, Gamepad gamepad)
+        {
+            yield return new WaitForSeconds(duration);
+            gamepad?.SetMotorSpeeds(0f, 0f);
+            _hapticCoroutine = null;
         }
     }
 }
