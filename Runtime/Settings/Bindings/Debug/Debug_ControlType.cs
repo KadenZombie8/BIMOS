aa7558dec3ba51e43b6c00c4bcdb97f809ae1824
KadenZombie8 <kadenzombie8@gmail.com>
2026-07-09 00:34:34 +0100

    Height forced to 180cm in flatscreen
    Variable height is only applied in VR.

--- Runtime/Settings/Bindings/Debug/Debug_ControlType.cs
+++ Runtime/Settings/Bindings/Debug/Debug_ControlType.cs
@@ -1,7 +1,9 @@
+using KadenZombie8.BIMOS.Rig;
 using KadenZombie8.BIMOS.Rig.Movement;
 using KadenZombie8.BIMOS.UI;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Management;
 
 namespace KadenZombie8.BIMOS.Settings.Bindings
@@ -17,10 +19,18 @@ namespace KadenZombie8.BIMOS.Settings.Bindings
         [SerializeField]
         private ScreenModeCamera _screenModeCamera;
 
+        [SerializeField]
+        private ControllerRig _controllerRig;
+
+        [SerializeField]
+        private string _realHeightKey = "VR_RealHeight";
+
         private void Start()
         {
             if (Setting.Value == 0)
                 StartCoroutine(StartXR());
+            else
+                _controllerRig.UpdateRealHeight(180f);
         }
 
         protected override void SettingSaved(int value)
@@ -35,6 +45,12 @@ namespace KadenZombie8.BIMOS.Settings.Bindings
         {
             _menuToggleFlatscreen.SetMenuOpen(false);
             _screenModeCamera.enabled = false;
+
+            BIMOSUtils.Settings.TryGetSetting(_realHeightKey, out var setting);
+            var heightSetting = (Setting<float>)setting;
+            var height = heightSetting.Load();
+            _controllerRig.UpdateRealHeight(height);
+
             var manager = XRGeneralSettings.Instance.Manager;
             if (manager.activeLoader) yield break;
             yield return manager.InitializeLoader();
@@ -44,6 +60,7 @@ namespace KadenZombie8.BIMOS.Settings.Bindings
 
         private void StopXR()
         {
+            _controllerRig.UpdateRealHeight(180f);
             _menuToggleVR.SetMenuOpen(false);
             _screenModeCamera.enabled = true;
             var manager = XRGeneralSettings.Instance.Manager;
